6c1788b7c14090a34f5956e383547106dfe23f51
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-24 20:38:38 -0300

    feat: structure box message and header

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Icon } from '@iconify/vue';
 import user_profile from '/img/user-empty-profile.jpg';
 </script>
 
@@ -11,15 +12,22 @@ import user_profile from '/img/user-empty-profile.jpg';
           alt="Foto do contato"
           class="rounded-full"
         />
-        <figcaption class="text-primary">
+        <figcaption class="text-primary w-9/12">
           Paulo Sérgio
         </figcaption>
+        <div class="action-icon float-left border-2 rounded p-3 text-xl">
+          <Icon icon="ic:round-more-vert" />
+        </div>
       </figure>
       <div class="px-6 pb-5 border-b-2">
-        <input type="text" placeholder="Encontre um contato" class="w-full h-10 border pl-2 rounded" />
+        <input
+          type="text"
+          placeholder="Encontre um contato"
+          class="w-full h-10 border pl-2 rounded"
+        />
       </div>
       <ul class="contacts-list">
-        <li class="flex pl-6  py-3">
+        <li class="flex pl-6 py-3">
           <div>
             <img
               :src="user_profile"
@@ -55,7 +63,7 @@ import user_profile from '/img/user-empty-profile.jpg';
             </div>
           </div>
         </li>
-        <li class="flex pl-6  py-3">
+        <li class="flex pl-6 py-3">
           <div>
             <img
               :src="user_profile"
@@ -76,11 +84,24 @@ import user_profile from '/img/user-empty-profile.jpg';
       </ul>
     </aside>
     <div class="w-3/4 px-6">
-      <header>
-        header do contato selecionado
+      <header class="flex py-5 items-center border-b-4">
+        <div>
+          <img
+            :src="user_profile"
+            alt="Foto do contato"
+            class="rounded-full"
+          />
+        </div>
+        <div class="title">
+          <h3>Nome do contato de uma conversa</h3>
+          <div id="status"></div>
+        </div>
+        <div>
+          Ações
+        </div>
       </header>
       <div>
-        <div>
+        <div style="height: 75dvh; background-color: blue;">
           caixa de conversa
         </div>
         <div>
@@ -95,6 +116,16 @@ import user_profile from '/img/user-empty-profile.jpg';
 </template>
 
 <style scoped>
+.action-icon {
+  margin-right: 14px;
+  cursor: pointer;
+}
+
+.action-icon:hover {
+  background-color: #e5e5e5;
+  transition: .2s ease-in-out;
+}
+
 img {
   margin-right: 14px;
   width: 55px;
@@ -107,5 +138,25 @@ figure figcaption {
 
 .contacts-list {
   margin-top: 20px;
+}
+
+header .title {
+  display: flex;
+  flex: 1;
+  align-items: center;
+}
+
+header .title h3 {
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: #333;
+}
+header .title #status {
+  width: 10px;
+  height: 10px;
+  padding: 5px;
+  background: rgb(0, 206, 0);
+  border-radius: 100%;
+  margin-left: 10px;
 }
 </style>
