85cedb17795fd20950790bee36666a4a4cc98ce0
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-24 10:12:03 -0300

    feat: add event emmiter for register new user

--- src/components/Forms/RegisterForm.vue
+++ src/components/Forms/RegisterForm.vue
@@ -1,22 +1,38 @@
 <script setup>
+import { ref, defineEmits } from 'vue';
 import MainButton from '@/components/MainButton/index.vue';
+
+const registerData = ref({
+  name: '',
+  email: '',
+  password: '',
+});
+
+const emit = defineEmits(['handleCreateUser']);
+
+const handleCreateUser = () => {
+  emit('handleCreateUser', registerData.value);
+};
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="handleCreateUser">
     <input
       type="text"
       placeholder="Insira seu nome"
+      v-model="registerData.name"
       class="w-full h-10 border pl-2 mb-2 rounded"
     />
     <input
       type="email"
       placeholder="Insira seu E-mail"
+      v-model="registerData.email"
       class="w-full h-10 border pl-2 mb-2 rounded"
     />
     <input
       type="password"
       placeholder="Insira sua senha"
+      v-model="registerData.password"
       class="w-full h-10 border pl-2 mb-2 rounded"
     />
     <MainButton class="bg-primary">
