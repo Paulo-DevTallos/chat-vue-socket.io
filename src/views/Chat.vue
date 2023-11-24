e96983ba047470d866c763a077eb906ecb8b0ac4
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-24 19:47:08 -0300

    style: structure styles for asidebar

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,22 +1,81 @@
 <script setup>
+import user_profile from '/img/user-empty-profile.jpg';
 </script>
 
 <template>
   <div class="w-full flex h-screen">
-    <aside style="background-color: red; width: 25%;">
-      <figure>
-        Aqui ficará a foto do usuário
-        Nome do usuario
-        <!-- <img src="" alt="Foto do usuário" />
-        <figcaption>
-          Nome do usuário
-        </figcaption> -->
+    <aside class="bg-grayslace w-1/4">
+      <figure class="flex items-center py-12 pl-6 h-16">
+        <img
+          :src="user_profile"
+          alt="Foto do contato"
+          class="rounded-full"
+        />
+        <figcaption class="text-primary">
+          Paulo Sérgio
+        </figcaption>
       </figure>
-      <ul>
-        <li>lista com os meus usuarios</li>
+      <div class="px-6 pb-5 border-b-2">
+        <input type="text" placeholder="Encontre um contato" class="w-full h-10 border pl-2 rounded" />
+      </div>
+      <ul class="contacts-list">
+        <li class="flex pl-6  py-3">
+          <div>
+            <img
+              :src="user_profile"
+              alt="Foto do contato"
+              class="rounded-full"
+            />
+          </div>
+          <div class="flex">
+            <div>
+              <h3>Nome do contato</h3>
+              <p>Última mensagem</p>
+            </div>
+            <div>
+              <p>10:35 AM</p>
+            </div>
+          </div>
+        </li>
+        <li class="flex pl-6 py-3">
+          <div>
+            <img
+              :src="user_profile"
+              alt="Foto do contato"
+              class="rounded-full"
+            />
+          </div>
+          <div class="flex">
+            <div>
+              <h3>Nome do contato</h3>
+              <p>Última mensagem</p>
+            </div>
+            <div>
+              <p>10:35 AM</p>
+            </div>
+          </div>
+        </li>
+        <li class="flex pl-6  py-3">
+          <div>
+            <img
+              :src="user_profile"
+              alt="Foto do contato"
+              class="rounded-full"
+            />
+          </div>
+          <div class="flex">
+            <div>
+              <h3>Nome do contato</h3>
+              <p>Última mensagem</p>
+            </div>
+            <div>
+              <p>10:35 AM</p>
+            </div>
+          </div>
+        </li>
       </ul>
     </aside>
-    <div style="background-color: yellow; width: 100%;">
+    <div class="w-3/4 px-6">
       <header>
         header do contato selecionado
       </header>
@@ -36,5 +95,17 @@
 </template>
 
 <style scoped>
+img {
+  margin-right: 14px;
+  width: 55px;
+}
 
+figure figcaption {
+  font-size: 1.3rem;
+  font-weight: 700;
+}
+
+.contacts-list {
+  margin-top: 20px;
+}
 </style>
