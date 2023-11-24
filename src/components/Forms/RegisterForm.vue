1e197fc87a0367d9a8e6694be269e3369cfa4c9e
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-24 09:47:47 -0300

    feat: create form register

---
+++ src/components/Forms/RegisterForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MainButton from '@/components/MainButton/index.vue';
+</script>
+
+<template>
+  <form>
+    <input
+      type="text"
+      placeholder="Insira seu nome"
+      class="w-full h-10 border pl-2 mb-2 rounded"
+    />
+    <input
+      type="email"
+      placeholder="Insira seu E-mail"
+      class="w-full h-10 border pl-2 mb-2 rounded"
+    />
+    <input
+      type="password"
+      placeholder="Insira sua senha"
+      class="w-full h-10 border pl-2 mb-2 rounded"
+    />
+    <MainButton class="bg-primary">
+      Cadastrar
+    </MainButton>
+  </form>
+</template>
