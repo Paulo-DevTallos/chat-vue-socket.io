30f0d5018d53a02fb778b5fd6e956477b0a975ca
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-25 17:34:36 -0300

    style: add styles for contacts list

--- src/components/ContactsList/index.vue
+++ src/components/ContactsList/index.vue
@@ -1,60 +1,59 @@
 <script setup>
+import { ref } from 'vue';
 import user_profile from '/img/user-empty-profile.jpg';
+
+const listContacts = ref([
+  {
+    id: 1,
+    name: 'Guilherme Martins',
+    last_message: 'Última mensagem env...',
+    time: '9:45 AM',
+    user_profile,
+  },
+  {
+    id: 2,
+    name: 'Aldo Souza',
+    last_message: 'Última mensagem',
+    time: '10:23 AM',
+    user_profile,
+  },
+  {
+    id: 3,
+    name: 'Luiz Brito',
+    last_message: 'Última mensagem',
+    time: '10:35 AM',
+    user_profile,
+  },
+]);
 </script>
 
 <template>
-  <ul class="mt-6">
-    <li class="flex pl-6 py-3">
+  <ul class="mt-6 px-6">
+    <li
+      v-for="contact in listContacts"
+      :key="contact.id"
+      class="flex px-1 py-3 hover:bg-slate-200 border-b-1 border-slate-200"
+    >
       <div>
         <img
-          :src="user_profile"
-          alt="Foto do contato"
+          :src="contact.user_profile"
+          :alt="`Foto do contato ${contact.name}`"
           class="rounded-full"
         />
       </div>
-      <div class="flex">
-        <div>
-          <h3>Nome do contato</h3>
-          <p>Última mensagem</p>
+      <div class="flex w-full">
+        <div class="pl-3" style="width: 80%;">
+          <h3 class="font-medium text-base text-primary mb-2">
+            {{ contact.name }}
+          </h3>
+          <p class="text-slate-500 leading-4 text-sm font-medium">
+            {{ contact.last_message }}
+          </p>
         </div>
-        <div>
-          <p>10:35 AM</p>
-        </div>
-      </div>
-    </li>
-    <li class="flex pl-6 py-3">
-      <div>
-        <img
-          :src="user_profile"
-          alt="Foto do contato"
-          class="rounded-full"
-        />
-      </div>
-      <div class="flex">
-        <div>
-          <h3>Nome do contato</h3>
-          <p>Última mensagem</p>
-        </div>
-        <div>
-          <p>10:35 AM</p>
-        </div>
-      </div>
-    </li>
-    <li class="flex pl-6 py-3">
-      <div>
-        <img
-          :src="user_profile"
-          alt="Foto do contato"
-          class="rounded-full"
-        />
-      </div>
-      <div class="flex">
-        <div>
-          <h3>Nome do contato</h3>
-          <p>Última mensagem</p>
-        </div>
-        <div>
-          <p>10:35 AM</p>
+        <div style="width: 20%;" class="text-right">
+          <small class="text-slate-500 font-medium">
+            {{ contact.time }}
+          </small>
         </div>
       </div>
     </li>
