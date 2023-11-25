0b6ecac3d966e5576f41adad4f86d78184999610
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-25 15:03:23 -0300

    feat: create baseInput component and implement password revealer

---
+++ src/components/BaseInput/index.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed, ref, defineProps } from 'vue';
+import PasswordRevealer from '@/components/Icons/PasswordRevealer.vue';
+
+const inputType = ref('password');
+const inputValue = ref('');
+
+const props = defineProps({
+  placeholder_msg: {
+    type: String,
+  },
+});
+
+const computedProps = {
+  isPasswordVisible: computed(() => inputType.value === 'text'),
+};
+
+const togglePassword = () => {
+  inputType.value = computedProps.isPasswordVisible.value ? 'password' : 'text';
+};
+</script>
+
+<template>
+  <div tabindex="0" class="w-full h-10 pl-2 rounded border input-container">
+    <input
+      v-bind="$attrs"
+      :type="inputType"
+      :placeholder="placeholder_msg"
+      class="set-input"
+    />
+    <PasswordRevealer
+      @toggleRevealer="togglePassword"
+      :is_password="computedProps.isPasswordVisible.value"
+    />
+  </div>
+</template>
+
+<style scoped>
+.input-container {
+  background: #fff;
+  display: flex;
+}
+
+.input-container:focus-within {
+  outline: 1px solid #004AAD;
+}
+
+.input-container .set-input {
+  width: 90%;
+  height: 100%;
+  outline: none;
+  padding: 0 8px;
+}
+</style>
