156d2e30d5d486d23451b17b40e6c43c9c3c1b09
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-25 15:02:37 -0300

    feat: create password revealer component

---
+++ src/components/Icons/PasswordRevealer.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineEmits, defineProps, ref } from 'vue';
+import { Icon } from '@iconify/vue';
+
+const props = defineProps({
+  is_password: {
+    type: Boolean,
+  },
+});
+
+const emit = defineEmits(['toggleRevealer']);
+
+const toggleRevealer = () => {
+  emit('toggleRevealer');
+};
+</script>
+
+<template>
+  <span @click="toggleRevealer" class="cursor-pointer flex items-center justify-center w-1/20">
+    <Icon icon="carbon:view" v-if="is_password" />
+    <Icon icon="carbon:view-off" v-else />
+  </span>
+</template>
