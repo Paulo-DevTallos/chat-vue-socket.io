4187ec8dfbe02610cc1f98294ddbd44dca435d57
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-26 12:58:11 -0300

    feat: add command user list

---
+++ src/configs/profile-actions.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'Editar perfil',
+    icon: 'outline-edit',
+    action: '',
+  },
+  {
+    id: 2,
+    name: 'Adicionar contato',
+    icon: 'outline-person-add',
+    action: '',
+  },
+  {
+    id: 3,
+    name: 'Lista de contatos',
+    icon: 'outline-library-books',
+    action: '',
+  },
+  {
+    id: 4,
+    name: 'Meus favoritos',
+    icon: 'outline-favorite-border',
+    action: '',
+  },
+];
