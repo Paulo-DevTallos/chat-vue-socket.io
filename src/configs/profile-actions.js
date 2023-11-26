423d389941fb11e63b2b34cc49018f85ca75c9fe
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-26 14:58:20 -0300

    feat: add event action

--- src/configs/profile-actions.js
+++ src/configs/profile-actions.js
@@ -3,24 +3,24 @@ export default [
     id: 1,
     name: 'Editar perfil',
     icon: 'outline-edit',
-    action: '',
+    action: 'Evento 1',
   },
   {
     id: 2,
     name: 'Adicionar contato',
     icon: 'outline-person-add',
-    action: '',
+    action: 'Evento 2',
   },
   {
     id: 3,
     name: 'Lista de contatos',
     icon: 'outline-library-books',
-    action: '',
+    action: 'Evento 3',
   },
   {
     id: 4,
     name: 'Meus favoritos',
     icon: 'outline-favorite-border',
-    action: '',
+    action: 'Evento 4',
   },
 ];
