c7f7bda6c005c792ecfb1a216f13d15eed8ff932
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-26 11:49:33 -0300

    feat: add menu icon for actions

---
+++ src/components/Icons/MenuIcon.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+
+const eventoDeClick = () => {
+  alert('Evento de click');
+};
+</script>
+
+<template>
+  <div
+    title="Editar perfil"
+    @click="eventoDeClick"
+    class="icon-menu border-2"
+  >
+    <Icon icon="ic:outline-edit" />
+  </div>
+</template>
+
+<style scoped>
+.icon-menu {
+  cursor: pointer;
+  color: #a8a8a8;
+  padding: 20px;
+  font-size: 1.5rem;
+  border-radius: 6px;
+  transition: .3s ease-in;
+}
+
+.icon-menu:hover {
+  border-color: #004AAD;
+  color: #004AAD;
+}
+</style>
